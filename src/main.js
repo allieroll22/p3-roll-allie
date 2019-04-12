30fcbb746fa1c63c48fc61065274cceec19f413b
“Allie <allieroll22@ufl.edu>
2019-04-12 16:43:02 -0400

    Add layer 2 and functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 //Global Variables
 const myButtons = document.getElementsByClassName('block');
+const mySecondButtons = document.getElementsByClassName('block-2');
+
+const layerOne = document.getElementById('layer-one');
+const layerTwo = document.getElementById('layer-two');
+
 
 //Functionality
 function loopThrough() {
@@ -10,7 +15,27 @@ function loopThrough() {
   }
 };
 
+function loopThroughNumber() {
+  const changeColor = () => this.classList.toggle('orange');
+  const numStringValue = this.textContent;
+  for ( let i =0; i < Number(numStringValue * 2); i++) {
+    setTimeout(changeColor, i * 1000);
+  }
+};
+
+//Change layer
+function changeLayer() {
+  const swap = () => {
+    layerOne.classList.add('hide');
+    layerTwo.classList.remove('hide');
+  };
+  setTimeout(swap, this.textContent.length * 2000);
+};
+
 //Event Listeners
 for (let i = 0; i < myButtons.length; i++) {
   myButtons[i].addEventListener('click', loopThrough);
+  myButtons[i].addEventListener('click', changeLayer);
+
+  mySecondButtons[i].addEventListener('click', loopThroughNumber);
 }
