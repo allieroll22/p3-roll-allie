84c3adfc10e42232db98993d4369a1274861ebc6
“Allie <allieroll22@ufl.edu>
2019-04-12 17:57:24 -0400

    Create last two layers in html and JS

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 //Global Variables
 const myButtons = document.getElementsByClassName('block');
 const mySecondButtons = document.getElementsByClassName('block-2');
-
+const myThirdButtons = document.getElementsByClassName('block-3');
+const myFourthButtons = document.getElementsByClassName('block-4');
 const layerOne = document.getElementById('layer-one');
 const layerTwo = document.getElementById('layer-two');
+const layerThree = document.getElementById('layer-three');
+const layerFour = document.getElementById('layer-four');
 
 
 //Functionality
 function loopThrough() {
   const changeColor = () => this.classList.toggle('orange');
 
-  for ( let i =0; i < this.textContent.length * 2; i++) {
+  for ( let i = 0; i < this.textContent.length * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
 };
@@ -18,7 +21,7 @@ function loopThrough() {
 function loopThroughNumber() {
   const changeColor = () => this.classList.toggle('orange');
   const numStringValue = this.textContent;
-  for ( let i =0; i < Number(numStringValue * 2); i++) {
+  for ( let i = 0; i < Number(numStringValue * 2); i++) {
     setTimeout(changeColor, i * 1000);
   }
 };
@@ -32,10 +35,30 @@ function changeLayer() {
   setTimeout(swap, this.textContent.length * 2000);
 };
 
+function changeLayerTwo() {
+  const swap = () => {
+    layerTwo.classList.add('hide');
+    layerThree.classList.remove('hide');
+  };
+  setTimeout(swap, this.textContent * 2000);
+};
+
+function changeLayerThree() {
+  const swap = () => {
+    layerThree.classList.add('hide');
+    layerFour.classList.remove('hide');
+  };
+  setTimeout(swap, this.textContent * 2000);
+
+};
+
 //Event Listeners
 for (let i = 0; i < myButtons.length; i++) {
   myButtons[i].addEventListener('click', loopThrough);
   myButtons[i].addEventListener('click', changeLayer);
-
   mySecondButtons[i].addEventListener('click', loopThroughNumber);
+  mySecondButtons[i].addEventListener('click', changeLayerTwo);
+  myThirdButtons[i].addEventListener('click', loopThroughNumber);
+  myThirdButtons[i].addEventListener('click', changeLayerThree);
+
 }
